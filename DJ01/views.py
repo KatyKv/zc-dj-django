c732cecac5b6cbb957f3f0c59b51650c94b330ce
Ekaterina <kate.kvacheva@gmail.com>
2025-05-30 01:40:33 +0500

    Initial commit: Django project structure and first task implementation (DJ01): different routes

---
+++ DJ01/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# ВАЖНО! request - обязательный параметр!
+def index(request):
+    return HttpResponse(
+        '<h1>Первый проект на Django</h1>'
+        '<p><a href="/data">Ссылка на страницу data</a></p>'
+        '<p><a href="/test">Ссылка на страницу test</a></p>'
+    )
+
+def new(request):
+    return HttpResponse('<h2>Вторая страница проекта</h2>')
+
+def datapage(request):
+    return HttpResponse(
+        '<h1>Страница данных</h1>'
+        '<h2>Здесь может быть какая-то информация</h2>'
+    )
+
+def testpage(request):
+    return HttpResponse(
+        '<h2>Тестовая страница</h2>'
+        '<p>Выведем расчёт:</p>'
+        f'<p>Число 2 в степени 128 =  {2 ** 128}<p>'
+    )
